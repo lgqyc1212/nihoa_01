5132cf06d60ba010076d93a93722713e3da01630
lgqyc1212 <2900238849@qq.com>
2019-10-25 20:45:16 +0800

    "静态页面"

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 export default new VueRouter({
-  routes: [
-
-  ]
+  routes: [{
+    path: '/login',
+    component: Login
+  }]
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <!-- 登录卡片 -->
+    <el-card>
+      <!-- logo -->
+      <img src="../../assets/logo_index.png" width="200px" alt />
+      <el-form ref="form" :model="loginFrm">
+        <el-form-item>
+          <el-input v-model="loginFrm.name" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginFrm.code"
+            style="width:235px; margin-right: 10px"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">立即登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginFrm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/login_bg.jpg) no-repeat center / cover;
+}
+.el-card {
+  width: 400px;
+  height: 340px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.el-card img {
+  display: block;
+  margin: 0 auto 30px;
+}
+</style>
